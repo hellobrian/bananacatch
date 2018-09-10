3c4c8aac4879bc8e2959bdf32b7eb440f6805325
Brian Han <brianhan87usa@gmail.com>
2018-09-09 19:22:19 -0500

    add michael cera as new start button

--- public/src.a2e20e7a.js
+++ public/src.a2e20e7a.js
@@ -19648,7 +19648,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54513' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53500' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
